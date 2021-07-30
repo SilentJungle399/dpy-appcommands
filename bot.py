a20ffeffa9e377bf82706a3a9952548a70747d3d
SilentJungle399 <silentjungle399@gmail.com>
2021-07-30 21:46:17 +0530

    cus i need it to test

--- bot.py
+++ bot.py
@@ -1,21 +1,28 @@
-
+from slash import SlashClient
 from discord.ext import commands
-import discord
+from discord import http
 import os
+import json
 
 class Bot(commands.Bot):
-    def __init__(self, **kwargs):
-        super().__init__(command_prefix=commands.when_mentioned_or('?'), **kwargs)
+	def __init__(self, **kwargs):
+		super().__init__(command_prefix=commands.when_mentioned_or('?'), **kwargs)
+		self.slashclient = SlashClient(self)
 
-    async def on_ready(self):
-        print(f'Logged on as {self.user} (ID: {self.user.id})')
+	async def on_ready(self):
+		print(f'Logged on as {self.user} (ID: {self.user.id})')
 
-    async def on_socket_response(self, data):
-        print(data)
-
+	async def on_socket_response(self, data):
+		if data['t'] == "INTERACTION_CREATE":
+			with open("ee.json", "w") as f1:
+				json.dump(data, f1)
 
 bot = Bot()
 
-# write general commands here
+@bot.command()
+async def aa(ctx):
+	print(await bot.slashclient.get_commands())
+
+
 
 bot.run("NzQwNTY4NzY2MTk4NDQ4MTkw.Xyq6aA.aEi-KSd_V9YL3UX9pg3hSDZFmoM")
