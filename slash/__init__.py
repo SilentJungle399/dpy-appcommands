d27c9a73a3bcb2b21c4d04d3dc6c7e8f6426a6f4
Rishidev <73101003+Rishiraj0100@users.noreply.github.com>
2021-08-14 12:28:38 +0530

    Update __init__.py

--- slash/__init__.py
+++ slash/__init__.py
@@ -2,5 +2,5 @@ __author__ = "SilentJungle399"
 __version__ = "0.4.0a"
 
 from .client import SlashClient
-from .models import SlashCommand, InteractionContext
+from .models import SlashCommand, InteractionContext, command
 from .enums import *
