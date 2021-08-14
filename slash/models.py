750003b5227a470a12e119cc5467107b06479f2c
Rishidev <73101003+Rishiraj0100@users.noreply.github.com>
2021-08-14 11:52:12 +0530

    Update models.py

--- slash/models.py
+++ slash/models.py
@@ -23,29 +23,25 @@ class InteractionContext:
 		self.channel: discord.TextChannel = None
 		self.guild: discord.Guild = None
 
-	async def from_dict(self, data: dict) -> 'InteractionContext':
-		self.version = data["version"]
-		self.type = data["type"]
-		self.token = data["token"]
-		self.id = int(data["id"])
-		self.data = data["data"]
-		self.application_id = int(data["application_id"])
+	async def from_interaction(self, interaction) -> 'InteractionContext':
+		self.version = interaction.version
+		self.type = interaction.type
+		self.token = interaction.token
+		self.id = interaction.id
+		self.data = interaction.data
+		self.application_id = interaction.application_id
+		self.user = interaction.user
+		self.guild = None
 
-		if "guild_id" in data:
-			self.guild = self.bot.get_guild(int(data["guild_id"]))
-			if not self.guild:
-				self.guild = await self.bot.fetch_guild(int(data["guild_id"]))
-
-		if "channel_id" in data:
-			self.channel = self.guild.get_channel(int(data["channel_id"]))
+		if isinstance(interaction.user, discord.Member):
+			self.guild = self.user.guild
+			
+		if interaction.channel_id is not None:
+			self.channel = self.guild.get_channel(interaction.channel_id)
 			if not self.channel:
-				self.channel = await self.guild.fetch_channel(int(data["channel_id"]))
+				self.channel = await self.guild.fetch_channel(interaction.channel_id)
 
-		if "member" in data:
-			self.member = self.guild.get_member(int(data["member"]["user"]["id"]))
-			if not self.member:
-				self.member = await self.guild.fetch_member(int(data["member"]["user"]["id"]))
-
+		
 		return self
 
 	async def reply(
