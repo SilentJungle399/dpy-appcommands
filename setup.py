f0b6f72899f7db0a24ef667ad55c627e41897b9f
Rishidev <73101003+Rishiraj0100@users.noreply.github.com>
2021-08-14 10:15:07 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,28 @@
 import setuptools
-import slash
+import re
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
+version = ''
+with open('slash/__init__.py') as f:
+    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(1)
+
+if version.endswith(('a', 'b', 'rc')):
+    # append version identifier based on commit count
+    try:
+        import subprocess
+        p = subprocess.Popen(['git', 'rev-list', '--count', 'HEAD'],
+                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        if out:
+            version += out.decode('utf-8').strip()
+        p = subprocess.Popen(['git', 'rev-parse', '--short', 'HEAD'],
+                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = p.communicate()
+        if out:
+            version += '+g' + out.decode('utf-8').strip()
+    except Exception:
+        pass
 
 setuptools.setup(
     name="slash",
