8b63ae34c6818256fed57cd510fcd5e826521f3f
SilentJungle399 <silentjungle399@gmail.com>
2021-09-02 15:19:00 +0530

    guild support, listeners now use ids instead of name

--- slash/__init__.py
+++ slash/__init__.py
@@ -1,6 +1,6 @@
 __author__ = "SilentJungle399"
 __version__ = "0.4.0a"
 
-from .client import SlashClient
+from .client import SlashClient, Bot
 from .models import *
 from .enums import *
--- slash/client.py
+++ slash/client.py
@@ -1,6 +1,8 @@
 import importlib
-from typing import Coroutine, List, Tuple, Union
+from logging import log
+from typing import Coroutine, List, Optional, Tuple, Union, Dict
 import discord
+from discord import interactions
 
 Item = discord.ui.Item
 from .models import InteractionContext, SlashCommand, command as _cmd
@@ -12,6 +14,19 @@ from discord.interactions import Interaction
 import sys
 import asyncio
 
+class Bot(commands.Bot):
+    # Remove comments and add docs
+
+    # - inherits commands.Bot
+    # - one extra attribute, slashclient, now inbuilt instead of being init by user previously
+    def __init__(self, command_prefix, help_command = None, description = None, **options):
+        super().__init__(command_prefix, help_command=help_command, description=description, **options)
+
+        self.slashclient = SlashClient(self, logging = True if options.get("slashlog") else False)
+
+    # just a shorthand
+    def slash(self, *args, **kwargs):
+        self.slashclient.command(*args, **kwargs)
 
 class SlashClient:
     """Slash Client handler class for bot
@@ -37,9 +52,13 @@ class SlashClient:
                 "Bot has already a slashclient registered with this module")
         self.bot.slashclient = self
         self.logging: bool = logging
-        self._listeners = {}
         self._views: Dict[str, Tuple[ui.View, Item]] = {}
+        self.__commands = {}
         self.bot.add_listener(self.socket_resp, "on_interaction")
+
+    @property
+    def commands(self):
+        return self.__commands
 
     def command(self, *args, **kwargs):
         """Adds a command to bot
@@ -98,10 +117,12 @@ class SlashClient:
 
     async def socket_resp(self, interaction):
         if interaction.type == InteractionType.application_command:
-            if interaction.data['name'] in self._listeners:
+            if int(interaction.data['id']) in self.__commands:
+                command = self.__commands[int(interaction.data['id'])]
                 context = await InteractionContext(
                     self.bot, self).from_interaction(interaction)
-                await (self._listeners[context.data["name"]]).callback(
+
+                await (command['command']).callback(
                     context, **context.kwargs)
 
         elif interaction.type == InteractionType.component:
@@ -113,12 +134,16 @@ class SlashClient:
             item.refresh_state(interactctx)
             view._dispatch_item(item, interactctx)
 
-    async def get_commands(self) -> List[SlashCommand]:
-        """Gives a list of slash command currently the bot have"""
+    async def fetch_commands(self, guild_id: Optional[int]) -> List[SlashCommand]:
+        """fetch a list of slash command currently the bot have"""
         while not self.bot.is_ready():
             await self.bot.wait_until_ready()
+        add = ""
+        if guild_id:
+            add = f"/guilds/{guild_id}"
+
         data = await self.bot.http.request(route=http.Route(
-            "GET", f"/applications/{self.bot.user.id}/commands"))
+            "GET", f"/applications/{self.bot.user.id}{add}/commands"))
         ret = []
         for i in data:
             if i["type"] == 1:
@@ -126,14 +151,24 @@ class SlashClient:
 
         return ret
 
-    def get_command(self, name: str):
+    def get_commands(self) -> dict[str, SlashCommand]:
+        """Gets every command registered in the current running instance"""
+        ret = {}
+
+        for i in self.__commands:
+            ret[self.__commands[i]['command'].name] = self.__commands[i]['command']
+
+        return ret
+
+    def get_command(self, name: str) -> SlashCommand:
         """Gives a command registered in this module
         
         Parameters
         -----------
         name: :class:`~str`
             the name from which command is to be found"""
-        return self._listeners.get(name)
+
+        return (self.get_commands()).get(name)
 
     async def add_command(self, command: SlashCommand):
         """Adds a slash command to bot
@@ -147,18 +182,34 @@ class SlashClient:
         -------
         .CommandExists
             That slash cmd is already registered in bot with this module"""
-        slashcmds = await self.get_commands()
-        if command.name in self._listeners:
+        slashcmds = await self.fetch_commands(None)
+
+        if command.guild:
+            slashcmds = await self.fetch_commands(command.guild)
+
+        if command.name in self.get_commands():
             raise CommandExists(
                 f"Command '{command.name}' has already been registered!")
         else:
             if command in slashcmds:
                 await self.remove_command(command.name)
-            await self.bot.http.request(route=http.Route(
-                "POST", f"/applications/{self.bot.user.id}/commands"),
+
+            add = ""
+            l_add = ""
+            if command.guild:
+                add = f"/guilds/{command.guild}"
+                l_add = f"Guild command for '{command.guild}'"
+
+            resp = await self.bot.http.request(route=http.Route(
+                "POST", f"/applications/{self.bot.user.id}{add}/commands"),
                                         json=command.ret_dict())
-            self._listeners[command.name] = command
-            self.log(f"Slash command '{command.name}' registered!")
+
+            self.log(f"Slash command '{command.name}' (ID: {resp['id']}) registered! {l_add}")
+
+            self.__commands[int(resp['id'])] = {
+                "guild": None if 'guild_id' not in resp else int(resp['guild_id']),
+                "command": command
+            }
 
     def reload_command(self, command: SlashCommand):
         """Reloads a slash command
@@ -172,30 +223,39 @@ class SlashClient:
         --------
         .CommandNotRegistered
             That command is not registered"""
-        if command.name not in self._listeners:
+        a = self.__commands
+        listenerlist = {self.__commands[i]['command'].name: i for i in self.__commands}
+        _id = listenerlist.get(command.name)
+
+        if not _id:
             raise CommandNotRegistered(
                 f"Command '{command.name}' has not been registered.")
         else:
-            self._listeners.pop(command.name)
-            self._listeners[command.name] = command
+            self.__commands.pop(_id)
+            self.__commands[_id] = {
+                "guild": command.guild,
+                "command": command
+            }
+
             self.log(f"Slash command '{command.name}' reloaded!")
 
     async def remove_command(self, name: str):
         """Removes command from the name given
-       
+
         Parameters
         ------------
         name: :class:`~str`
             Name of the command"""
-        slashcmds = await self.get_commands()
-        checks = list(map(lambda a: a.name, slashcmds))
-        if name not in checks:
+        listenerlist = {self.__commands[i]['command'].name: i for i in self.__commands}
+        _id = listenerlist.get(name)
+
+        if not _id:
             raise CommandDoesNotExists(f"Command '{name}' does not exist!")
         else:
-            id = slashcmds[checks.index(name)].id
-
             await self.bot.http.request(route=http.Route(
                 "DELETE", f"/applications/{self.bot.user.id}/commands/{id}"))
+
+            self.__commands.pop(_id)
 
     def load_extension(self, name: str):
         spec = importlib.util.find_spec(name)
