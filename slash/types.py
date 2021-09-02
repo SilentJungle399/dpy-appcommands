f46db4d09c06e84e97a687ae1b7d0e61b1dd958d
SilentJungle399 <silentjungle399@gmail.com>
2021-09-02 19:42:26 +0530

    misc

--- slash/types.py
+++ slash/types.py
@@ -1,5 +1,6 @@
+from types import FunctionType
 from .enums import MessageFlags
-from typing import Coroutine, Dict, List, Tuple
+from typing import Coroutine, Dict, List, Optional, Tuple
 from discord.ext import commands
 import discord
 from discord import ui
@@ -58,9 +59,13 @@ class InteractionContext:
 		...
 
 class SlashCommand:
-	name: str
-	options: dict
 	client: 'SlashClient'
+	name: str
+	description: str
+	options: dict
+	guild: Optional[int]
+	options: Optional[List['Option']]
+	callback: Optional[FunctionType]
 
 	def from_dict(self, data: dict) -> 'SlashCommand':
 		...
@@ -84,3 +89,14 @@ class SlashClient:
 
 	def log(self, message: str):
 		...
+
+class Option:
+	name: str
+	description: Optional[str]
+	type: Optional[str]
+	required: Optional[bool]
+	choices: Optional[List['Choice']]
+
+class Choice:
+	name: str
+	value: Optional[str]
