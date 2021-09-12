199472aa1186f41f0d300d1e35542dbcaa32fede
RishiDev <73101003+Rishiraj0100@users.noreply.github.com>
2021-09-12 12:20:45 +0530

    Update cog.py

--- appcommands/cog.py
+++ appcommands/cog.py
@@ -116,7 +116,8 @@ class SlashCog(Cog):
             func.__slash__ = new_cmd
             bot.loop.create_task(bot.appclient.add_command(func.__slash__))
             new_list.append(cmd)
-            setattr(self, new_cmd.name, new_cmd.callback)
+            setattr(self, new_cmd.callback.__name__, new_cmd.callback)
+            setattr(self.__class__, new_cmd.callback.__name__, func)
 
         self.__slash_commands__ = tuple(c for c in new_list)
         return super()._inject(bot)
